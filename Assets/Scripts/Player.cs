f22e72241d65c1fa0ac2ab74f9972cbc2c114811
Alex Deane <alexrdeane@hotmail.com>
2019-05-07 11:20:51 +1000

    code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+public float gravity = -10f;
+private CharacterController2D controller;
+
     // Start is called before the first frame update
     void Start()
     {
+controller = GetComponent(typeof(CharacterController));
         
     }
 
