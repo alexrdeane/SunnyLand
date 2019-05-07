2dd06d4ad78e1dc45e507aba0d2e1c9c1d9892a1
Alex Deane <alexrdeane@hotmail.com>
2019-05-07 11:54:37 +1000

    scripts work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using Prime31; 
+
 public class Player : MonoBehaviour
 {
 public float gravity = -10f;
 private CharacterController2D controller;
+public float moveSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-controller = GetComponent(typeof(CharacterController));
-        
+        controller = GetComponent<CharacterController2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	float inputH = Input.GetAxis("Horizontal");
+	float inputW = Input.GetAxis("Vertical");
+	controller.move(transform.right * inputH * (moveSpeed * Time.deltaTime));        
     }
 }
