f8c52516a504278231500382ec7f7b4d4591aaeb
alexrdeane <alexrdeane@hotmail.com>
2019-05-28 12:03:50 +1000

    animation adding

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using Prime31; 
+using Prime31;
 
 public class Player : MonoBehaviour
 {
-public float gravity = -10f;
-private CharacterController2D controller;
-public float moveSpeed = 10f;
+    public float gravity = -10f;
+    private CharacterController2D controller;
+    public float moveSpeed = 10f;
 
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
+
         controller = GetComponent<CharacterController2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-	float inputH = Input.GetAxis("Horizontal");
-	float inputW = Input.GetAxis("Vertical");
-	controller.move(transform.right * inputH * (moveSpeed * Time.deltaTime));        
+        float inputH = Input.GetAxis("Horizontal");
+        float inputW = Input.GetAxis("Vertical");
+        Move(inputH, inputW);
+    }
+
+    private void Move(float inputH, float inputW)
+    {
+        controller.move(transform.right * inputH * (moveSpeed * Time.deltaTime));
+        bool isRunning = inputH != 0;
+        anim.SetBool("isRunning", true);
     }
 }
