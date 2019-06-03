de2e96976885559a77a2a130af12ca9bcbd35f46
Alex Deane <alexrdeane@hotmail.com>
2019-06-03 12:01:22 +1000

    ladderWIP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,25 @@ public class Player : MonoBehaviour
     public float gravity = -10f;
     public float moveSpeed = 2.5f;
     public float jumpheight = 2.5f;
+    public float centreRadius = 0.1f;
 
     private SpriteRenderer rend;
     private Animator anim;
     private CharacterController2D controller;
     private Vector3 velocity;
+    private bool isClimbing = false;
 
     void Start()
     {
         rend = GetComponent<SpriteRenderer>();
         controller = GetComponent<CharacterController2D>();
         anim = GetComponent<Animator>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, centreRadius);
     }
 
     void Update()
@@ -48,13 +56,13 @@ public class Player : MonoBehaviour
         anim.SetFloat("jumpY", velocity.y);
 
         //plays run void
-        Run(inputH);
+        Move(inputH);
         Climb(inputV);
         //applies velocity to controller (to get it to move)
         controller.move(velocity * Time.deltaTime);
     }
 
-    private void Run(float inputH)
+    void Move(float inputH)
     {
         //move player left and right
         controller.move(transform.right * inputH * (moveSpeed * Time.deltaTime));
@@ -71,11 +79,33 @@ public class Player : MonoBehaviour
     private void Climb(float inputV)
     {
 
+        bool isOverLadder = false;
+        //get list of all hits objects overlapping ladde
+        Collider2D[] hits = Physics2D.OverlapPointAll(transform.position);
+        //loop through each point
+        foreach (var hit in hits)
+        {
+            //if the point overlaps a climbable object
+            if(hit.tag == "Ladder")
+            {
+                //player is overlapping ladder
+                isOverLadder = true;
+                break;
+            }
+        }
+        //is climbing
+        // if is climbing
+        //perform logic for climbing
     }
 
     void Jump()
     {
         //sets velocity on the y axis to jump height
         velocity.y = jumpheight;
+    }
+
+    public void Hurt()
+    {
+
     }
 }
